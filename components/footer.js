6110f96a82251d85cb3ecfff6004d7c09e7014a7
Mattias Gulec <Voltair88@gmail.com>
2022-09-08 12:52:13 +0200

    tried SPA with only javascript but desided to not go with it.

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,5 @@
 const Footer = `
-        <a href="www.github.com/voltair88"><p>made by Mattias Gulec</p></a>
+        <a href="www.github.com/voltair88"><p>Made by Mattias Gulec</p></a>
     `;
 
 window.addEventListener("DOMContentLoaded", () => {
