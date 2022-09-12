96be7910d2b3cf249d067f849f8d4598438dd929
Mattias Gulec <Voltair88@gmail.com>
2022-09-12 15:24:28 +0200

    lighthouse fixes

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,3 @@
-const Footer = `
-        <a href="www.github.com/voltair88"><p>Made by Mattias Gulec</p></a>
-    `;
-
-window.addEventListener("DOMContentLoaded", () => {
-  let foot = document.querySelector('footer[role="footer"]');
-  foot.innerHTML = Footer;
-});
+document.getElementById("Footer").innerHTML = `
+  <a href="www.github.com/voltair88"><p>Made by Mattias Gulec</p></a>
+  `;
