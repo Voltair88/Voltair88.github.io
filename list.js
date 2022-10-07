2d0b76aefcdc5ede5b094d95f80d0c2776b44009
Mattias Gulec <Voltair88@gmail.com>
2022-10-07 20:16:12 +0200

    added localstorage and fixing delete button onload

--- list.js
+++ list.js
@@ -1,16 +1,53 @@
-const addlist = document.getElementById("addlistitem");
-const list = document.getElementById("list");
-const input = document.getElementById("input");
-const add = document.getElementById("addlist");
+// Language: javascript
+function list() {
+  const addlist = document.getElementById("addlistitem");
+  const list = document.getElementById("list");
+  const input = document.getElementById("input");
+  const add = document.getElementById("addlist");
 
-add.onclick = () => {
-  if (input.value === "") {
-    alert("Please enter a value");
-  } else {
-    console.log(input.value);
+  add.onclick = () => {
     const li = document.createElement("li");
+    li.setAttribute("id", "listitem" + list.children.length);
     li.innerHTML = input.value;
     list.appendChild(li);
     input.value = "";
+    input.focus();
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute("class", "delete");
+    deleteButton.innerHTML = "Delete";
+    li.appendChild(deleteButton);
+    deleteButton.onclick = () => {
+      li.parentNode.removeChild(li);
+      localStorage.setItem("list", list.innerHTML);
+      console.log("delete");
+    };
+    localStorage.setItem("list", list.innerHTML);
+  };
+
+  for (let i = 0; i < deleteButton.length; i++) {
+    deleteButton[i].onclick = () => {
+      const li = deleteButton[i].parentElement;
+      li.parentNode.removeChild(li);
+      localStorage.setItem("list", list.innerHTML);
+      console.log("delete");
+    };
   }
-};
+
+  // load delete button from localstorage and add event listener
+  deleteButton = document.getElementsByClassName("delete");
+
+  for (let i = 0; i < deleteButton.length; i++) {
+    deleteButton[i].onclick = () => {
+      const li = deleteButton[i].parentElement;
+      li.parentNode.removeChild(li);
+      localStorage.setItem("list", list.innerHTML);
+      console.log("delete");
+    };
+  }
+
+  if (localStorage.getItem("list")) {
+    list.innerHTML = localStorage.getItem("list");
+  }
+}
+
+list();
