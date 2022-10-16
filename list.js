bf4709c876326b62c1ba033b20e59c0455206a08
Mattias Gulec <Voltair88@gmail.com>
2022-10-16 17:14:36 +0200

    removed console log

--- list.js
+++ list.js
@@ -2,4 +2,3 @@ const list = document.getElementById("list");
 const input = document.getElementById("input");
 const add = document.getElementById("addlist");
 const config = require;
-console.log("loaded");
